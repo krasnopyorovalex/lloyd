deca8552b1e3c2b0a8054617fee36ce313440907
Sanek <djShtaket88@mail.ru>
2019-04-01 17:49:41 +0300

    add default title if empty

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>@yield('title', 'Мебель для гостиниц')</title>
+    <title>@yield('title', 'Русспецкоут - специализированная лакокрасочная компания')</title>
     <meta name="description" content="@yield('description', '')">
     <meta name="google" content="notranslate">
     <meta name="csrf-token" content="{{ csrf_token() }}">
