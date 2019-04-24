85b9598bb805057c279ceaf2ccfa02a12ff63a87
Sanek <djShtaket88@mail.ru>
2019-04-24 16:22:55 +0300

    task #1648: remove attr initial scale

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>@yield('title', '- Бикор Техно - специализированная лакокрасочная компания')</title>
     <meta name="description" content="@yield('description', '')">
