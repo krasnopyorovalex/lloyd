b811377dba76cec12a88630af0629faf45cb6420
Sanek <djShtaket88@mail.ru>
2019-08-15 09:40:15 +0300

    remove tag robots noindex\nofollow from layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>@yield('title', '- Бикор Техно - специализированная лакокрасочная компания')</title>
     <meta name="description" content="@yield('description', '')">
-    <meta name="robots" content="noindex, nofollow" />
     <meta name="google" content="notranslate">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#eee">
