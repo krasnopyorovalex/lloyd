97f8a7aadaa6a5b067265ebffe29dc79a69eebf1
a.krasnopyorov <a.krasnoperov@easy-sms.ru>
2020-07-02 20:59:23 +0300

    update slider

--- resources/views/layouts/sections/slider.blade.php
+++ resources/views/layouts/sections/slider.blade.php
@@ -3,7 +3,7 @@
     <div class="owl-carousel owl-theme main-slider">
         @foreach($slider->images as $image)
             <div class="item">
-                <img src="{{ asset($image->getPath()) }}" alt="{{ $image->alt }}" title="{{ $image->title }}">
+                <img src="{{ asset($image->getPath()) }}" alt="{{ $image->alt }}">
                 <div class="camera_caption">
                     <h2>{{ $image->name }}</h2>
                 </div>
