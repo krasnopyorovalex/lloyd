0b9960842d48a918fe2220420ed3a4f68e88d842
a.krasnopyorov <a.krasnoperov@easy-sms.ru>
2020-07-14 09:27:16 +0300

    add schema to images

--- resources/views/layouts/sections/projects.blade.php
+++ resources/views/layouts/sections/projects.blade.php
@@ -4,20 +4,19 @@
         <div class="row">
             @foreach($projectsInMain as $project)
                 <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="box2">
+                    <div class="box2" itemscope itemtype="http://schema.org/ImageObject">
                         <a class="thumb" href="{{ $project->url }}">
                             @if($project->image)
-                            <img src="{{ $project->image->path }}" alt="{{ $project->image->alt }}" title="{{ $project->image->title }}">
+                            <img src="{{ $project->image->path }}" alt="{{ $project->image->alt }}" title="{{ $project->image->title }}" itemprop="contentUrl">
                             <span class="thumb_overlay"></span>
                             @endif
                         </a>
 
                         <div class="box2_cnt">
                             <h5 class="strong">
-                                <a href="{{ $project->url }}">{{ $project->name }}</a>
+                                <a href="{{ $project->url }}" itemprop="name">{{ $project->name }}</a>
                             </h5>
-
-                            <p class="big">{{ strip_tags($project->preview) }}</p>
+                            <p class="big" itemprop="description">{{ strip_tags($project->preview) }}</p>
                         </div>
                     </div>
                 </div>
