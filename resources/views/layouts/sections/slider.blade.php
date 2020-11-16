53b282ef77a83b37db70ba0c67b26009dc85ef8d
Александр Краснопёров <krasnopyorovalex88@gmail.com>
2020-11-16 22:21:49 +0300

    add link to slider image

--- resources/views/layouts/sections/slider.blade.php
+++ resources/views/layouts/sections/slider.blade.php
@@ -5,12 +5,14 @@
                 <div class="col-12">
                     <div class="slider">
                         @foreach($slider->images as $image)
+                            @if($image->link && $image->link !== '/') <a href="{{ $image->link }}">@endif
                             <div class="slide" itemscope itemtype="http://schema.org/ImageObject">
                                 <img src="{{ asset($image->getPath()) }}" alt="{{ $image->alt }}" itemprop="contentUrl">
                                 <div class="slide-text">
                                     <h2 itemprop="name">{{ $image->name }}</h2>
                                 </div>
                             </div>
+                            @if($image->link) </a>@endif
                         @endforeach
                     </div>
                 </div>
